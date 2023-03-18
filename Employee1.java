3795ff6c1d6ed73b060f33597c400a93d76ea03d
B153Prem <sirsikarp@gmail.com>
2023-03-18 17:44:38 +0530

    New file created By Prem

---
+++ Employee1.java
@@ -0,0 +1,30 @@
+public void Employee1
+{
+
+   int eid;
+   String ename;
+ public static void main(String [] args)
+ {
+    Employee1 e=new Employee();
+       e.eid=101;
+       e.ename="Prem"
+
+    System.out.println(e.eid);
+    System.out.println(e.ename);
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
